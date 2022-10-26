348821dad7d074d4af837a4f1bb5b5e09acf22df
Marwin Wongjarupun <marwin111@gmail.com>
2022-10-26 05:16:23 +0100

    added new scenes

---
+++ Assets/Scripts/Putter.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Putter : MonoBehaviour
+{
+    [SerializeField]
+    private Transform head;
+
+    [SerializeField] private int speed = 5;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 normal = (collision.transform.position - head.position).normalized;
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(normal * speed, ForceMode.Impulse);
+    }
+}
